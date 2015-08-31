07ce12f24f9310f3420e8b06d377d5dd7e0311aa
sethdame <seth_dame@yahoo.com>
2015-08-31 17:49:53 -0600

    third commit

--- js/AmazonController.js
+++ js/AmazonController.js
@@ -2,6 +2,7 @@ var app = angular.module('AmazonApp');
 
 app.controller('AmazonController', function($scope, AmazonService){
 
+	$scope.addPrice = '';
 	$scope.sortType = '';
 
 	$scope.data = AmazonService.getData();
